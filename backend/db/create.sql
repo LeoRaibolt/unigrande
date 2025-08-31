9cf4256fe98e793bd11f3882cba60fa1aadb852a
Leo Raibolt <leonardoramosraibolt@gmail.com>
2025-08-30 22:22:02 -0300

    feat: completando o Create e Dockerfile

--- backend/db/create.sql
+++ backend/db/create.sql
@@ -1,5 +1,2 @@
-# Pull official base image for PostgreSQL 15.10
-FROM postgres:15.10
- 
-# Run create.sql on init
-ADD create.sql /docker-entrypoint-initdb.d
+CREATE DATABASE unigrande_db;
+CREATE DATABASE unigrande_db_test;
